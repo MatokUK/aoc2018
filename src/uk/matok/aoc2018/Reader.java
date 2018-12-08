48d3aa7d3e64ed92ef626b977762cfa5a00e007c
Matej Kuna <mat.kuna@gmail.com>
2018-12-08 17:30:17 +0100

    init commit

---
+++ src/uk/matok/aoc2018/Reader.java
@@ -0,0 +1,33 @@
+package uk.matok.aoc2018;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Reader
+{
+    private String fileName;
+
+    public Reader(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public ArrayList<Integer> read() throws IOException {
+        ArrayList<Integer> values = new ArrayList<Integer>();
+
+                //uses the class loader search mechanism:
+        InputStream input = this.getClass().getResourceAsStream(this.fileName);
+        InputStreamReader isr = new InputStreamReader(input);
+        BufferedReader reader = new BufferedReader(isr);
+//        ){
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                values.add(new Integer(line));
+            }
+    //    }
+
+        return values;
+    }
+}
